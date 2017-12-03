c50e295b9b8911f8da08dc763d1fd608c3c7a1c8
Игорь Кузнецов <iikuznetsov_3@edu.hse.ru>
2017-12-03 16:49:02 +0300

    Добавьте файлы проекта.

---
+++ Read_every_day/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Read_every_day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
